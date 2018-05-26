64fef0de91395bf16a1de8f540e341778302d916
Ben Woodworth <ben@benwoodworth.net>
2018-05-25 20:09:56 -0400

    Implemented MatrixProduct

--- src/net/benwoodworth/polyart/matrices/Matrix.kt
+++ src/net/benwoodworth/polyart/matrices/Matrix.kt
@@ -1,35 +1,28 @@
 package net.benwoodworth.polyart.matrices
 
-interface Matrix<TValue, TRows : MatrixSize, TColumns : MatrixSize> {
+interface Matrix<TRows : MatrixSize, TColumns : MatrixSize> {
+    val rows: Int
+    val columns: Int
 
-    val rows: TRows
-    val columns: TColumns
+    operator fun get(row: Int, column: Int): Double
 
-    val determinant: TValue
-
-    val transpose: Matrix<TValue, TColumns, TRows>
-
-    operator fun get(row: Int, column: Int): TValue
-
-    fun <TArgColumns : MatrixSize> dot(
-            matrix: Matrix<TValue, TColumns, TArgColumns>
-    ): Matrix<TValue, TRows, TArgColumns>
-
-    operator fun plus(matrix: Matrix<TValue, TRows, TColumns>): Matrix<TValue, TRows, TColumns>
-
-    operator fun times(value: TValue): Matrix<TValue, TRows, TColumns>
-
-    operator fun unaryPlus(): Matrix<TValue, TRows, TColumns>
-
-    operator fun unaryMinus(): Matrix<TValue, TRows, TColumns>
-
-    companion object {
-        fun create<TValue, TRows, TColumns>(
-                rows: TRows,
-                columns: TColumns,
-                vararg values: TValue
-        ){
-            return
-        }
-    }
+//    val determinant: Double
+//
+//    val transpose: Matrix<TColumns, TRows>
+//
+//    fun <TArgColumns : MatrixSize> crossProduct(
+//            matrix: Matrix<TColumns, TArgColumns>
+//    ): Matrix<TRows, TArgColumns>
+//
+//    infix fun <TArgColumns : MatrixSize> x(
+//            matrix: Matrix<TColumns, TArgColumns>
+//    ): Matrix<TRows, TArgColumns>
+//
+//    operator fun plus(matrix: Matrix<TRows, TColumns>): Matrix<TRows, TColumns>
+//
+//    operator fun times(multiplier: Double): Matrix<TRows, TColumns>
+//
+//    operator fun unaryPlus(): Matrix<TRows, TColumns>
+//
+//    operator fun unaryMinus(): Matrix<TRows, TColumns>
 }
---
+++ src/net/benwoodworth/polyart/matrices/MatrixProduct.kt
@@ -0,0 +1,49 @@
+package net.benwoodworth.polyart.matrices
+
+@Suppress("UNCHECKED_CAST")
+private class MatrixProduct<TRows : MatrixSize, TMatching : MatrixSize, TColumns : MatrixSize>(
+        private val matrixA: Matrix<TRows, TMatching>,
+        private val matrixB: Matrix<TMatching, TColumns>
+) : Matrix<TRows, TColumns> {
+
+    init {
+        require(matrixA.columns == matrixB.rows) {
+            "The number of columns in matrixA (${matrixA.columns}) " +
+                    "must equal the number of rows in matrixB (${matrixB.rows})."
+        }
+    }
+
+    override val rows = matrixA.rows
+    override val columns = matrixB.columns
+
+    val values = DoubleArray(rows * columns)
+    val valuesLoaded = BooleanArray(values.size)
+
+    override fun get(row: Int, column: Int): Double {
+        val valueIndex = row * columns + column
+
+        if (valuesLoaded[valueIndex]) {
+            return values[valueIndex]
+        }
+
+        var result = 0.0
+        for (i in 0 until matrixA.columns) {
+            val a = matrixA[row, i]
+            if (a == 0.0) continue
+
+            val b = matrixB[i, column]
+            result += a * b
+        }
+
+        values[valueIndex] = result
+        valuesLoaded[valueIndex] = true
+
+        return result
+    }
+}
+
+operator fun <TRows : MatrixSize, TMatching : MatrixSize, TColumns : MatrixSize> Matrix<TRows, TMatching>.times(
+        matrix: Matrix<TMatching, TColumns>
+): Matrix<TRows, TColumns> {
+    return MatrixProduct(this, matrix)
+}
